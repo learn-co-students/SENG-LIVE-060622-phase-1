b4304df87586d53fb31b50a5ee4bfeef15845766
ixnp <enprocopios@gmail.com>
2022-06-13 13:39:16 -0700

    patch and delete

--- 06_PATCH_and_DELETE_Requests/src/index.js
+++ 06_PATCH_and_DELETE_Requests/src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     // Fetch requests 
         // Function for making a GET request 
         function fetchResource(url){
@@ -30,26 +31,47 @@ document.addEventListener('DOMContentLoaded', () => {
             footerDivs[2].textContent = store.hours
         }
     
-        function renderBookCard(cardData) {
+        function renderBookCard(cardData){
+            
             const li = document.createElement('li')
             const h3 = document.createElement('h3')
             const pAuthor = document.createElement('p')
             const pPrice = document.createElement('p')
+            const inputInv = document.createElement('input')
             const img = document.createElement('img')
             const btn = document.createElement('button')
     
             h3.textContent = cardData.title
             pAuthor.textContent = cardData.author
             pPrice.textContent = `$${cardData.price}`
+            inputInv.value = cardData.inventory
+            inputInv.type = 'number'
             btn.textContent = 'Delete'
     
             img.src = cardData.imageUrl
             li.className = 'list-li'
     
             //Event Listeners 
-            btn.addEventListener('click',()=>li.remove())
+            inputInv.addEventListener('change', (e) => {
+                console.log(e.target.value)
+                fetch(`http://localhost:3000/books/${cardData.id}`,{
+                    method:'PATCH',
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                    body: JSON.stringify({inventory:e.target.value})
+                })
+            })
+
+            btn.addEventListener('click',()=>{
+                fetch(`http://localhost:3000/books/${cardData.id}`,{
+                    method:'DELETE'
+                })
+                .then(() => li.remove())
+            })
         
-            li.append(h3,pAuthor,pPrice,img,btn)
+            li.append(h3,pAuthor,pPrice, inputInv, img, btn)
+            //.appendChild
             document.querySelector('#book-list').append(li)
         }
     
@@ -72,18 +94,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     
     
-    // Invoking functions    
-        fetchResource('http://localhost:3000/stores/1')
+    // // Invoking functions  
+        // fetchResource('http://localhost:3000/stores')
+        // .then(data => {
+        //     data.forEach((store) => {
+        //         const p = document.createElement('p')
+        //         p.textContent = store.name
+        //         document.querySelector('main').prepend(p)
+        //         p.addEventListener('click', () => {
+        //             fetchResource(`http://localhost:3000/stores/${store.id}`)
+        //             .then(storeJson => {
+        //                 renderHeader(storeJson)
+        //                 renderFooter(storeJson)
+        //             })
+        //         })
+        //     } )
+        // })
+       
+
+        fetchResource('http://localhost:3000/stores/2')
         .then(store => {
             renderHeader(store)
             renderFooter(store)
         })
         .catch(e => console.error(e))
     
+    // //////
         fetchResource('http://localhost:3000/books')
         .then(books => books.forEach(renderBookCard))
         .catch(e => console.error(e))
     
-        document.querySelector('#book-form').addEventListener('submit', handleForm)
+         document.querySelector('#book-form').addEventListener('submit', handleForm)
     
 })
